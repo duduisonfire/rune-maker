83682ad4596ec227be476744fdd755a4dff817cc
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-03-18 00:40:48 -0300

    Created rune interface and rune page interface.

---
+++ src/interfaces/IRune.ts
@@ -0,0 +1,5 @@
+export default interface IRune {
+  attribs: {
+    alt: string;
+  };
+}
---
+++ src/interfaces/IRunePage.ts
@@ -0,0 +1,20 @@
+export default interface IRunePage {
+  autoModifiedSelections: [];
+  current: boolean;
+  id: number;
+  isActive: boolean;
+  isDeletable: boolean;
+  isEditable: boolean;
+  isRecommendationOverride: boolean;
+  isTemporary: boolean;
+  isValid: boolean;
+  lastModified: number;
+  name: string;
+  order: number;
+  primaryStyleId: number;
+  recommendationChampionId: number;
+  recommendationIndex: number;
+  runeRecommendationId: '';
+  selectedPerkIds: number;
+  subStyleId: number;
+}
