ab39fab9a4d497bc72b6aebbcea21040b0d85745
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-03-18 00:57:06 -0300

    Created post and delete method.

--- src/libs/axiosConfig.ts
+++ src/libs/axiosConfig.ts
@@ -15,6 +15,34 @@ class lolRequest {
 
     return responseResult;
   }
+
+  static async delete(url: string) {
+    const lockfileData = JSON.parse(localStorage.getItem('lockfileData') as string) as ILockfileData;
+
+    const responseResult = axios.delete(url, {
+      baseURL: `https://127.0.0.1:${lockfileData.port}`,
+      auth: {
+        username: 'riot',
+        password: `${lockfileData.password}`,
+      },
+    });
+
+    return responseResult;
+  }
+
+  static async create(url: string, body: unknown) {
+    const lockfileData = JSON.parse(localStorage.getItem('lockfileData') as string) as ILockfileData;
+
+    const responseResult = axios.post(url, body, {
+      baseURL: `https://127.0.0.1:${lockfileData.port}`,
+      auth: {
+        username: 'riot',
+        password: `${lockfileData.password}`,
+      },
+    });
+
+    return responseResult;
+  }
 }
 
 export default lolRequest;
