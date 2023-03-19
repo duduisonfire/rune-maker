1b579c9719e9fec9d30215248b5152890a4e344a
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-03-19 02:11:47 -0300

    refactored eslint

--- src/pages/openedClient/index.tsx
+++ src/pages/openedClient/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MatchesContainer } from './styles/MatchesContainer';
 import { useNavigate } from 'react-router-dom';
 import { isOpen } from '../../libs/isOpen';
