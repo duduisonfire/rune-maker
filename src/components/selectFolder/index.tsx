ea8375df26adc80f6160607265c92741951aedaf
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-04-12 09:18:51 -0300

    Refactored SelectFolder and Index to otimize tests

--- src/components/selectFolder/index.tsx
+++ src/components/selectFolder/index.tsx
@@ -2,9 +2,9 @@ import React, { MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ElectronApi from '../../libs/ElectronApi';
 
-export default function SelectFolder(): JSX.Element {
+export default function SelectFolder(props: { electron: ElectronApi }): JSX.Element {
   const navigate = useNavigate();
-  const electron = new ElectronApi();
+  const electron = props.electron;
 
   async function getDir(e: MouseEvent) {
     const file = document.getElementById('file-input') as HTMLElement;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SelectFolder from '../../components/selectFolder';
 import { Container } from './styles/container';
+import ElectronApi from '../../libs/ElectronApi';
 
 export default function Index(): JSX.Element {
+  const electron = new ElectronApi();
+
   return (
     <Container>
-      <SelectFolder />
+      <SelectFolder electron={electron} />
     </Container>
   );
 }
