5879943166c360fb190c9abda990111ced084828
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-04-15 09:23:12 -0300

    Refactored haveFolder module.

--- src/routes/modules/haveFolder.tsx
+++ src/routes/modules/haveFolder.tsx
@@ -3,15 +3,17 @@
 
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import ElectronApi from '../../libs/ElectronApi';
 
 export default function HaveFolder({ ...rest }) {
   if (!localStorage.getItem('Lockfile')) {
     localStorage.setItem('Lockfile', '');
   }
 
-  window.lockfile.setFile(localStorage.getItem('Lockfile'));
-  window.lockfile.watch();
-  window.lockfile.openHandles();
+  const electron = new ElectronApi();
+  electron.setLeagueOfLegendsPath();
+  electron.watch();
+  electron.openHandle();
 
   const haveFolder = localStorage.getItem('Lockfile') === '';
   return haveFolder ? <Outlet {...rest} /> : <Navigate to="/closed" />;
