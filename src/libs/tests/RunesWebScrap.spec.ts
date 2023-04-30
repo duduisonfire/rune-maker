756f2b22fe0db62dcc3a777a92bda9e6624885b4
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-04-29 23:54:29 -0300

    Deleted a repeat test.

--- src/libs/tests/RunesWebScrap.spec.ts
+++ src/libs/tests/RunesWebScrap.spec.ts
@@ -2,13 +2,6 @@ import '@testing-library/jest-dom';
 import RuneWebScrap from '../RunesWebScrap';
 
 describe('RunesWebScrap Lib Tests', () => {
-  it('should be get rune list from u.gg with length 11', async () => {
-    const webScrap = new RuneWebScrap('aatrox', 'top');
-    await webScrap.selectRunes();
-
-    expect(webScrap.runeList.length).toBe(11);
-  });
-
   it('RuneWebScrap.runeList should be an array with length "11"', async () => {
     const webScrap = new RuneWebScrap('aatrox', 'top');
     await webScrap.selectRunes();
